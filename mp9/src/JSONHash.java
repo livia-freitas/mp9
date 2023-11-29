b992539aea3bcdb53c512c0d637db9f59c9fd966
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-11-28 20:37:05 -0600

    Add files via upload

---
+++ mp9/src/JSONHash.java
@@ -0,0 +1,96 @@
+package src;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+/**
+ * JSON hashes/objects.
+ */
+public class JSONHash implements JSONValue {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  // +-------------------------+-------------------------------------
+  // | Standard object methods |
+  // +-------------------------+
+
+  /**
+   * Convert to a string (e.g., for printing).
+   */
+  public String toString() {
+    return "";          // STUB
+  } // toString()
+
+  /**
+   * Compare to another object.
+   */
+  public boolean equals(Object other) {
+    if (other instanceof JSONHash){
+      // if this.equals(other) return true;
+    }
+    return true;        // STUB
+  } // equals(Object)
+
+  /**
+   * Compute the hash code.
+   */
+  public int hashCode() {
+    return this.hashCode(); 
+  } // hashCode()
+
+  // +--------------------+------------------------------------------
+  // | Additional methods |
+  // +--------------------+
+
+  /**
+   * Write the value as JSON.
+   */
+  public void writeJSON(PrintWriter pen) {
+                        // STUB
+  } // writeJSON(PrintWriter)
+
+  /**
+   * Get the underlying value.
+   */
+  public Iterator<KVPair<JSONString,JSONValue>> getValue() {
+    return this.iterator();
+  } // getValue()
+
+  // +-------------------+-------------------------------------------
+  // | Hashtable methods |
+  // +-------------------+
+
+  /**
+   * Get the value associated with a key.
+   */
+  public JSONValue get(JSONString key) {
+    return null;        // STUB
+  } // get(JSONString)
+
+  /**
+   * Get all of the key/value pairs.
+   */
+  public Iterator<KVPair<JSONString,JSONValue>> iterator() {
+    return null;        // STUB
+  } // iterator()
+
+  /**
+   * Set the value associated with a key.
+   */
+  public void set(JSONString key, JSONValue value) {
+                        // STUB
+  } // set(JSONString, JSONValue)
+
+  /**
+   * Find out how many key/value pairs are in the hash table.
+   */
+  public int size() {
+    return 0;           // STUB
+  } // size()
+
+} // class JSONHash
